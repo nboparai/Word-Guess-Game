9cde64ebae035e1c9f3c7fe0c9b65d7227ea6f5a
nboparai <navreet.boparai@yahoo.com>
2018-08-18 23:44:14 -0700

    2nd commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,29 @@
-
 var words=["lion", "cat", "deer", "dog", "wolf", "zebra", "elephant", "monkey"];
 var targetWord = words[Math.floor(Math.random()*words.length)];
 var answer = [];
-for (let i=0; i<targetWord.length; i++)
-    {
-        answer[i]="_";
+for (let i = 0; i < targetWord.length; i++) {
+    answer.push("_");
     
+}
+var remainingGuesses;
+remainingGuesses=targetWord.length;
+document.getElementById("lives").textContent=remainingGuesses;
+document.getElementById("hold").textContent=answer.join(" ");
+
+
+function letterChosen(letter) {  
+
+console.log(letter);
+for (let j = 0; j < remainingGuesses; j++) {
+    if (letter=targetWord[j]) {
+        answer[j]=letterChosen;
+        remainingGuesses--;
+    } 
+        
     }
-var remainingGuesses=targetWord.length;
-
-document.getElementById("lives").textContent=targetWord.length;
-while(remainingGuesses>0){
-    answer=(answer.join(" "));
-document.getElementById("hold").textContent=answer;
-
-// documnet.getElementById("hold").innerHTML=answer;
+document.onkeyup=function(event){
+    if (event.keyCode>=65 && event.keyCode<=99){
+        letterChosen(event.key);
+    }
+}
 }
